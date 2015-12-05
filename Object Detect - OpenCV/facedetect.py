3d2fa53e222147bfc5b5d4d801d6bfb68084aab1
senthilsnat <senthilsnat@yahoo.com>
2015-12-05 15:04:29 -0600

    face detect with still images

--- Object Detect - OpenCV/facedetect.py
+++ Object Detect - OpenCV/facedetect.py
@@ -1,31 +1,62 @@
 import sys
 import cv2
 
-#cascade = open('/Users/Senthil/Downloads/FaceDetect-master/haarcascade_frontalface_default.xml')
-faceCascade = cv2.CascadeClassifier('/Users/Senthil/Downloads/FaceDetect-master/haarcascade_frontalface_default.xml')
+cascade = '/Users/Senthil/Documents/elec-301/Object Detect - OpenCV/haarcascade_frontalface_default.xml'
+faceCascade = cv2.CascadeClassifier(cascade)
 
-videoFeed = cv2.VideoCapture(0)
+image1 = '/Users/Senthil/Documents/elec-301/images/senthil1.jpg'
 
-while True:
-    ret, frame = videoFeed.read()
-    grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def faceDetectWebcam():
+
+    videoFeed = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = videoFeed.read()
+        grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face = faceCascade.detectMultiScale(
+
+            grayImg,
+            scaleFactor = 1.2,
+            minNeighbors = 5,
+            minSize = (30,30),
+            flags = cv2.CASCADE_SCALE_IMAGE
+
+        )
+
+        for (x, y, w, h) in face:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    videoFeed.release()
+    cv2.destroyAllWindows()
+
+
+def faceDetectTiming(images, display = False):
+    image = cv2.imread(images)
+    grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
     face = faceCascade.detectMultiScale(
-
-        grayImg,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (30,30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-
-    )
+            grayImg,
+            scaleFactor = 1.2,
+            minNeighbors = 5,
+            minSize = (30,30),
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
 
     for (x, y, w, h) in face:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    cv2.imshow('Video', frame)
+    cv2.imshow("Faces found", image)
+
+    cv2.waitKey(0)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.destroyAllWindows()
 
-videoFeed.release()
-cv2.destroyAllWindows()
+
+faceDetectTiming(image1)
+
