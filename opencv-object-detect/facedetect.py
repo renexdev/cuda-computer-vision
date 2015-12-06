93432bfb7dec62d1d70bddbffc3ce07921e8dafa
senthilsnat <senthilsnat@yahoo.com>
2015-12-06 13:17:08 -0600

    face detect cleanup

--- opencv-object-detect/facedetect.py
+++ opencv-object-detect/facedetect.py
@@ -9,6 +9,7 @@ faceCascade = cv2.CascadeClassifier(cascade)
 image1 = '/Users/Senthil/Documents/elec-301/images/birthday1.jpg'
 daftPunk = cv2.imread('/Users/Senthil/Downloads/DaftPunk.png', cv2.IMREAD_UNCHANGED)
 
+
 def faceDetectWebcam():
 
     videoFeed = cv2.VideoCapture(0)
@@ -19,20 +20,20 @@ def faceDetectWebcam():
         face = faceCascade.detectMultiScale(
 
             grayImg,
-            scaleFactor = 1.2,
-            minNeighbors = 5,
-            minSize = (30,30),
-            flags = cv2.CASCADE_SCALE_IMAGE
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(30,30),
+            flags=cv2.CASCADE_SCALE_IMAGE
 
         )
 
         for (x, y, w, h) in face:
 
             recog = cv2.resize(daftPunk, (w, h), interpolation=cv2.INTER_AREA)
-            #print(recog.shape, recog.shape[0], recog.shape[1], w, h)
-            for c in range(0,3):
-                frame[y:y+h, x:x+w, c] = recog[:,:,c] * (recog[:,:,3]/255.0) +  frame[y:y+h, x:x+w, c] * (1.0 - recog[:,:,3]/255.0)
-            #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            # print(recog.shape, recog.shape[0], recog.shape[1], w, h)
+            for c in range(0, 3):
+                frame[y:y+h, x:x+w, c] = recog[:, :, c] * (recog[:, :, 3]/255.0) + frame[y:y+h, x:x+w, c] * (1.0 - recog[:, :, 3]/255.0)
+            # cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         cv2.imshow('Video', frame)
 
@@ -43,8 +44,8 @@ def faceDetectWebcam():
     cv2.destroyAllWindows()
 
 
-#train classifiers
-#train to recognize rich b's face, and apply a daft punk mask
+# train classifiers
+# train to recognize rich b's face, and apply a daft punk mask
 
 def faceDetectTiming(images):
     image = cv2.imread(images, cv2.IMREAD_UNCHANGED)
@@ -54,10 +55,10 @@ def faceDetectTiming(images):
 
     face = faceCascade.detectMultiScale(
             grayImg,
-            scaleFactor = 1.1,
-            minNeighbors = 5,
-            minSize = (30,30),
-            flags = cv2.CASCADE_SCALE_IMAGE
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
         )
 
     end_time = dt.datetime.now()
@@ -65,11 +66,11 @@ def faceDetectTiming(images):
     print("detected in:" + " " + str(time) + "ms")
 
     for (x, y, w, h) in face:
-        recog = cv2.resize(daftPunk, (w, h), interpolation=cv2.INTER_AREA)
-        #print(recog.shape, recog.shape[0], recog.shape[1], w, h)
-        #for c in range(0,3):
-        #    image[y:y+h, x:x+w, c] = recog[:,:,c] * (recog[:,:,3]/255.0) +  image[y:y+h, x:x+w, c] * (1.0 - recog[:,:,3]/255.0)
-        #image[y:y+h, x:x+w] = recog
+        # recog = cv2.resize(daftPunk, (w, h), interpolation=cv2.INTER_AREA)
+        # print(recog.shape, recog.shape[0], recog.shape[1], w, h)
+        # for c in range(0,3):
+        #   image[y:y+h, x:x+w, c] = recog[:,:,c] * (recog[:,:,3]/255.0) + image[y:y+h, x:x+w, c] * (1.0 - recog[:,:,3]/255.0)
+        # image[y:y+h, x:x+w] = recog
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     cv2.imshow("Faces found", image)
@@ -80,5 +81,5 @@ def faceDetectTiming(images):
         cv2.destroyAllWindows()
 
 
-#faceDetectTiming(image1)
+# faceDetectTiming(image1)
 faceDetectWebcam()
