7b92018242e5003fe268fc15e821dd5e519869d5
senthilsnat <senthilsnat@yahoo.com>
2015-12-06 13:03:29 -0600

    Add Daft Punk mask

--- opencv-object-detect/facedetect.py
+++ opencv-object-detect/facedetect.py
@@ -1,11 +1,13 @@
 import sys
 import cv2
 import datetime as dt
+import numpy
 
-cascade = '/Users/Senthil/Documents/elec-301/Object Detect - OpenCV/haarcascade_frontalface_default.xml'
+cascade = '/Users/Senthil/Documents/elec-301/opencv-object-detect/haarcascade_frontalface_default.xml'
 faceCascade = cv2.CascadeClassifier(cascade)
 
-image1 = '/Users/Senthil/Documents/elec-301/images/friends1.jpg'
+image1 = '/Users/Senthil/Documents/elec-301/images/birthday1.jpg'
+daftPunk = cv2.imread('/Users/Senthil/Downloads/DaftPunk.png', cv2.IMREAD_UNCHANGED)
 
 def faceDetectWebcam():
 
@@ -25,7 +27,12 @@ def faceDetectWebcam():
         )
 
         for (x, y, w, h) in face:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            recog = cv2.resize(daftPunk, (w, h), interpolation=cv2.INTER_AREA)
+            #print(recog.shape, recog.shape[0], recog.shape[1], w, h)
+            for c in range(0,3):
+                frame[y:y+h, x:x+w, c] = recog[:,:,c] * (recog[:,:,3]/255.0) +  frame[y:y+h, x:x+w, c] * (1.0 - recog[:,:,3]/255.0)
+            #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         cv2.imshow('Video', frame)
 
@@ -37,10 +44,10 @@ def faceDetectWebcam():
 
 
 #train classifiers
+#train to recognize rich b's face, and apply a daft punk mask
 
 def faceDetectTiming(images):
-    image = cv2.imread(images)
-
+    image = cv2.imread(images, cv2.IMREAD_UNCHANGED)
     start_time = dt.datetime.now()
 
     grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -55,10 +62,14 @@ def faceDetectTiming(images):
 
     end_time = dt.datetime.now()
     time = (end_time-start_time).microseconds
-
     print("detected in:" + " " + str(time) + "ms")
 
     for (x, y, w, h) in face:
+        recog = cv2.resize(daftPunk, (w, h), interpolation=cv2.INTER_AREA)
+        #print(recog.shape, recog.shape[0], recog.shape[1], w, h)
+        #for c in range(0,3):
+        #    image[y:y+h, x:x+w, c] = recog[:,:,c] * (recog[:,:,3]/255.0) +  image[y:y+h, x:x+w, c] * (1.0 - recog[:,:,3]/255.0)
+        #image[y:y+h, x:x+w] = recog
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     cv2.imshow("Faces found", image)
@@ -69,5 +80,5 @@ def faceDetectTiming(images):
         cv2.destroyAllWindows()
 
 
-faceDetectTiming(image1)
-
+#faceDetectTiming(image1)
+faceDetectWebcam()
