2fc09c64aaf99efe6c2aeca907133077341ccb3e
Kevin Lin <developer@kevinlin.info>
2015-12-07 22:38:04 +0000

    started face recognizer with CUDA

---
+++ opencv-object-detect/cuda-facedetect.cu
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+using namespace cv;
+using namespace std;
+using namespace cv::cuda;
+
+#include <opencv2/gpu/gpu.hpp>
+#include <vector>
+
+int main()
+{
+    printf("\n Hello World\n");
+    return EXIT_SUCCESS;
+}
+
+
+
+
