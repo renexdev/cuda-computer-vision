a608fd6ca7c2aea4403633ea147b93a6cd70d26a
Emilio <taikodrummer2@gmail.com>
2015-12-08 13:01:18 -0600

    fully implemented motin detection

--- opencv-python/motion_detection.py
+++ opencv-python/motion_detection.py
@@ -4,24 +4,43 @@ from matplotlib import pyplot as plt
 import edge_detect as edge
 
 # define constants
-M = 8  # number of columns
+M = 6  # number of columns
 N = 10  # number of rows
-T = 0.005  # Threshold
+T = 0.01  # Threshold
+B = 12  # movement tolerance
+
 
 # start video capture
 cap = cv2.VideoCapture(0)
 if not(cap.isOpened()):
     cap.open()
 
+# create gaussian filter size 3x3
+kernel = np.ones((3, 3), np.float32)/9
+
 # initialize last frame and edges of last frame
-last_frame = np.zeros((480, 640), np.uint8)
-last_frame_edge = edge.edge_detect(last_frame)[0]
+last_frame_edge = np.zeros((480, 640), np.uint8)
+
+
+def difference(frame1, frame2):
+    height, width = np.shape(frame1)
+    diff = np.zeros((height, width), np.uint8)
+    for i in range(height):
+        for j in range(width):
+            if frame1[i, j] != frame2[i, j]:
+                diff[i, j] = 255
+                for i_prime in range(-B, B):
+                    for j_prime in range(-B, B):
+                        if i+i_prime < height and i+i_prime >= 0 and j+j_prime < width and j+j_prime >= 0:
+                            if frame1[i+i_prime, j+j_prime] == frame2[i+i_prime, j+j_prime]:
+                                diff[i+i_prime, j+j_prime] = 0
+    return diff
 
 
 # motion detection algorithm
 def detect_motion(edge, last_edge):
     height, width, depth = np.shape(frame)
-    d = last_edge - edge
+    d = difference(edge, last_edge)
     motion = np.zeros((M, N), np.uint8)
     for i in range(M):
         for j in range(N):
@@ -37,15 +56,16 @@ def detect_motion(edge, last_edge):
 # main loop
 while cap.isOpened():
     ret, frame = cap.read()
-    frame_edge = edge.edge_detect(frame)
+    frame_filt = cv2.filter2D(frame, -1, kernel)
+    frame_edge = edge.edge_detect(frame_filt)
 
     # cv2.imshow('normal', frame)  # normal video
-    cv2.imshow('edge', frame_edge)  # edge detection video
+    cv2.imshow('edge', frame_edge)  # edge detection video)
     motion = detect_motion(frame_edge, last_frame_edge)
     cv2.imshow('motion', cv2.resize(motion, (640, 480)))  # motion detection video
+    # cv2.imshow('difference', difference(frame_edge, last_frame_edge))
 
     # update frames
-    last_frame = frame
     last_frame_edge = frame_edge
 
     # listen for exit
